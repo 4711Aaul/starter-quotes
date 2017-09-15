8a7c10606f13e6a3309ddaac9b75ba0ada736bdc
unhouseledutraquist <andrewlbusto@gmail.com>
2017-09-15 15:21:51 -0700

    Updated actor.php so that it contains the actor's info and quote.  Also slightly restructured the changelog. -AB

--- application/views/actor.php
+++ application/views/actor.php
@@ -1,8 +1,15 @@
 <div class="row">
         <div class="span4">
+            <div>
                 <a href="/welcome/show/{key}">
                         <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
                 </a>
+                <div> 
+                    {who} plays {role}
+                    <br>
+                    "{what}"
+                </div>
+            </div>
         </div>
 </div>
 
